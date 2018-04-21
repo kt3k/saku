fa76de654c6a917ba81d91fb45853640a3b50e79
Yoshiya Hinosawa <stibium121@gmail.com>
2018-04-22 02:35:25 +0900

    feat: add cli options

--- main.go
+++ main.go
@@ -14,6 +14,38 @@ func main() {
 
 	app.Version = Version
 
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "i, info",
+			Usage: "show the task information and exits",
+		},
+
+		cli.BoolFlag{
+			Name:  "p, parallel",
+			Usage: "run tasks in parallel",
+		},
+
+		cli.BoolFlag{
+			Name:  "r, race",
+			Usage: "set the flag to kill all tasks when a task finished with zero. This option is valid only with 'parallel' option",
+		},
+
+		cli.BoolFlag{
+			Name:  "s, serial",
+			Usage: "runs tasks sequentially",
+		},
+
+		cli.BoolFlag{
+			Name:  "q, quiet",
+			Usage: "stop logging",
+		},
+
+		cli.StringFlag{
+			Name: "c, config",
+			Usage: "Specifies the config file. Default is 'saku.md'.",
+		},
+	}
+
 	err := app.Run(os.Args)
 
 	if err != nil {
