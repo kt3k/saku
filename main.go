40d96b5ed3b7507c28838dda0afcb17fc3990a36
Yoshiya Hinosawa <stibium121@gmail.com>
2018-04-21 22:50:35 +0900

    chore: add format settings

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-  "log"
-  "os"
+	"log"
+	"os"
 
-  "github.com/urfave/cli"
+	"github.com/urfave/cli"
 )
 
 func main() {
-  err := cli.NewApp().Run(os.Args)
-  if err != nil {
-    log.Fatal(err)
-  }
+	err := cli.NewApp().Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
