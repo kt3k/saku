b78edeeaf3ce4b95fe12ed99e1ed3eace705ce34
Yoshiya Hinosawa <stibium121@gmail.com>
2018-04-22 20:49:25 +0900

    feat: add usage

---
+++ usage.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Shows the help message.
+func Usage() {
+	fmt.Println(`
+Usage: saku [options] <task, ...> [-- extra-options]
+
+Options:
+  -v, --version   - - - Shows the version number and exits.
+  -h, --help  - - - - - Shows the help message and exits.
+  -i, --info  - - - - - Shows the task information and exits.
+  -p, --parallel  - - - Runs tasks in parallel. Default false.
+  -s, --sequential  - - Runs tasks in serial. Default true.
+  -c, --config <path> - Specifies the config file. Default is 'saku.md'.
+  -r, --race  - - - - - Set the flag to kill all tasks when a task
+                        finished with zero. This option is valid only
+                        with 'parallel' option.
+  -q, --quiet   - - - - Stops the logging.
+
+The extra options after '--' are passed to each task command.
+`)
+}
