c72975cbe95381ff934c59a0736baa78536ed404
Yoshiya Hinosawa <stibium121@gmail.com>
2018-04-24 15:01:05 +0900

    docs: modify help output

--- usage.go
+++ usage.go
@@ -7,20 +7,20 @@ import (
 // Shows the help message.
 func Usage() {
 	fmt.Println(`
-Usage: saku [options] <task, ...> [-- extra-options]
+  Usage: saku [options] <task, ...> [-- extra-options]
 
-Options:
-  -v, --version   - - - Shows the version number and exits.
-  -h, --help  - - - - - Shows the help message and exits.
-  -i, --info  - - - - - Shows the task information and exits.
-  -p, --parallel  - - - Runs tasks in parallel. Default false.
-  -s, --sequential  - - Runs tasks in serial. Default true.
-  -c, --config <path> - Specifies the config file. Default is 'saku.md'.
-  -r, --race  - - - - - Set the flag to kill all tasks when a task
-                        finished with zero. This option is valid only
-                        with 'parallel' option.
-  -q, --quiet   - - - - Stops the logging.
+  Options:
+    -v, --version   - - - Shows the version number and exits.
+    -h, --help  - - - - - Shows the help message and exits.
+    -i, --info  - - - - - Shows the task information and exits.
+    -p, --parallel  - - - Runs tasks in parallel. Default false.
+    -s, --sequential  - - Runs tasks in serial. Default true.
+    -c, --config <path> - Specifies the config file. Default is 'saku.md'.
+    -r, --race  - - - - - Set the flag to kill all tasks when a task
+                          finished with zero. This option is valid only
+                          with 'parallel' option.
+    -q, --quiet   - - - - Stops the logging.
 
-The extra options after '--' are passed to each task command.
+  The extra options after '--' are passed to each task command.
 `)
 }
