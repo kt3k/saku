fa33d3464ab3131ee31dfbdf49f64aa1f79964ad
Yoshiya Hinosawa <stibium121@gmail.com>
2018-04-27 18:59:39 +0900

    feat: add task methods

--- task.go
+++ task.go
@@ -1,22 +1,33 @@
 package main
 
 type Task struct {
-	name        string
+	title       string
 	description string
 	commands    []string
 	options     TaskOptions
 	aborted     bool
 }
 
+func NewTask() *Task {
+	return &Task{
+		title:       "",
+		description: "",
+		commands:    []string{},
+		options:     TaskOptions{},
+		aborted:     false,
+	}
+}
+
 type TaskOptions struct {
 }
 
 // Runs a single command
-func (t *Task) RunSingle() {
+func (*Task) RunSingle(command string) {
 }
 
 // Runs a task
 func (t *Task) Run(opts *RunOptions) {
+	// TODO: runs all commands
 }
 
 // Aborts a task
@@ -24,4 +35,19 @@ func (t *Task) Abort() {
 	if t.aborted {
 		return
 	}
+}
+
+// Adds the description.
+func (t *Task) AddDescription(description string) {
+	t.description = t.description + description
+}
+
+// Sets the title.
+func (t *Task) SetTitle(title string) {
+	t.title = title
+}
+
+// Adds the code.
+func (t *Task) AddCommands(commands []string) {
+	t.commands = append(t.commands, commands...)
 }
