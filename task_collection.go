a7702eaa978fa4a79337df52e726b4732dbc35de
Yoshiya Hinosawa <stibium121@gmail.com>
2018-04-28 22:53:51 +0900

    refactor: change the visibility of task collection

--- task_collection.go
+++ task_collection.go
@@ -1,41 +1,41 @@
 package main
 
 // Task collection model.
-type taskCollection struct {
+type TaskCollection struct {
 	currentTask *task
 	tasks       []task
 }
 
 // Creates a new task collection.
-func newTaskCollection() *taskCollection {
+func newTaskCollection() *TaskCollection {
 
 	t := newTask()
 
-	return &taskCollection{
+	return &TaskCollection{
 		currentTask: &t, // This is a dummy task, and will be discarded when the first task is created
 		tasks:       []task{},
 	}
 }
 
-func (tc *taskCollection) Run() error {
+func (tc *TaskCollection) Run() error {
 	println("tasks running")
 
 	return &taskError{message: "Method not implemented"}
 }
 
-func (tc *taskCollection) newTask() {
+func (tc *TaskCollection) newTask() {
 	tc.tasks = append(tc.tasks, newTask())
 	tc.currentTask = &tc.tasks[len(tc.tasks)-1]
 }
 
-func (tc *taskCollection) setCurrentTaskTitle(title string) {
+func (tc *TaskCollection) setCurrentTaskTitle(title string) {
 	tc.currentTask.setTitle(title)
 }
 
-func (tc *taskCollection) addCurrentTaskDescription(description string) {
+func (tc *TaskCollection) addCurrentTaskDescription(description string) {
 	tc.currentTask.addDescription(description)
 }
 
-func (tc *taskCollection) addCurrentTaskCommands(commands []string) {
+func (tc *TaskCollection) addCurrentTaskCommands(commands []string) {
 	tc.currentTask.addCommands(commands)
 }
